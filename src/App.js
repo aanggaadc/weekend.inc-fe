9ac1c922838420649f8d994e567ee4fe20ccf2e7
unknown <aanggaadc@gmail.com>
2022-07-16 12:11:31 +0700

    initial commit and package

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "react-toastify/dist/ReactToastify.css";
+import {ToastContainer} from 'react-toastify'
+import {Routes, Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <>
+    <Routes>
+  
+    </Routes>
+    <ToastContainer />
+    </>
+  );
+}
+
+export default App;
